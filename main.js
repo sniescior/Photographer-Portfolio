caf6dd1ce14ea8ca21effe08f36dd7edabff7274
PseudonymSN <sniescior160@gmail.com>
2021-08-26 11:34:16 +0200

    commit

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
 function toggleMenu() {
 
     var menuToggle = document.querySelector('.menu');
-    var header = document.querySelector('.main-header');
     var container = document.querySelector('.container');
-    var gallery = document.querySelector('.gallery');
     var button  = document.querySelector('.button');
-    var buttonToggle  = document.querySelector('.toggle');
 
     menuToggle.classList.toggle('toggled');
     container.classList.toggle('active');
-    gallery.classList.toggle('active');
-    if(!header.classList.contains('scrolled')) {
-        header.classList.toggle('toggled');
-    } else {
-        buttonToggle.classList.toggle('click');
-    }
 
     if(button.classList.contains('icon-menu')) {
         button.classList.remove('icon-menu');
@@ -42,27 +33,3 @@ document.addEventListener('click', (event) => {
         toggleMenu();
     }
 });
-
-var wrapper = document.querySelector('.wrapper');
-var menu = document.querySelector('.menu');
-
-wrapper.addEventListener('scroll', () => {
-    let header = document.querySelector('.main-header');
-
-    // started scrolling
-    if(wrapper.scrollTop > 100) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    } 
-
-    // scrolled to the bottom
-    // if(wrapper.scrollTop === window.innerHeight) {} 
-
-    if(header.classList.contains('scrolled')) {
-        if(menu.classList.contains('toggled')) toggleMenu();
-    } else {
-        if(menu.classList.contains('toggled')) toggleMenu();
-    }
-});
-
