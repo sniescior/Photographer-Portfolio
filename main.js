1af0892f6f654a1d7795933daaad0fc7d4d62b8a
PseudonymSN <sniescior160@gmail.com>
2021-08-30 13:38:13 +0200

    commit

--- main.js
+++ main.js
@@ -63,6 +63,12 @@ const controller = new ScrollMagic.Controller();
 var x = $('#image-cards').innerWidth() + $(window.innerWidth);
 
 var scrollMagicTo = $('#image-cards').outerWidth();
+console.log(scrollMagicTo);
+if(scrollMagicTo < 1000) {
+    scrollMagicTo = scrollMagicTo*3;
+}
+
+console.log(scrollMagicTo);
 
 tl.from("#image-cards", 5, {x:0});
 // tl.to("#image-cards", 50, {x:-($('#image-cards').outerWidth())});  // <--- this kinda works
