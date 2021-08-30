4c0d9ed42358801ec461f2203cf0a233f0151483
PseudonymSN <sniescior160@gmail.com>
2021-08-30 12:41:19 +0200

    commit

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ const controller = new ScrollMagic.Controller();
 var x = $('#image-cards').innerWidth() + $(window.innerWidth);
 
 tl.from("#image-cards", 5, {x:100});
-tl.to("#image-cards", 50, {x:-($('#image-cards').innerWidth() + $('.container').innerWidth())});
+tl.to("#image-cards", 50, {x:-($('#image-cards').innerWidth() - $('.container').width())});
 
 const scene = new ScrollMagic.Scene({
     triggerElement: "#triggerElement",
