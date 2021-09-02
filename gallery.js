67488c1d08e7e7dbb431172c130f9215ca1077df
PseudonymSN <sniescior160@gmail.com>
2021-09-02 22:06:33 +0200

    commit

--- gallery.js
+++ gallery.js
@@ -1,3 +1,22 @@
 function clickedImage() {
     console.log('image clicked');
 }
+
+function sameParent(button, card) {
+    if(button.parentNode.id == card.parentNode.parentNode.id) {
+        return true;
+    }
+    return false;
+}
+
+function showMore(button) {
+
+    var cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        if(sameParent(button, card)) {
+            card.classList.remove('hidden');
+            card.classList.add('shown');
+        }
+    });
+}
