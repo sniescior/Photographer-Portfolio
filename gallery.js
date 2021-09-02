c41326c9d270bc63c13ee4ec8bbc41ca3cb55be0
PseudonymSN <sniescior160@gmail.com>
2021-09-02 22:41:24 +0200

    commit

--- gallery.js
+++ gallery.js
@@ -1,22 +1,58 @@
 function clickedImage() {
+    // function to do something when user clicks an image
     console.log('image clicked');
 }
 
 function sameParent(button, card) {
-    if(button.parentNode.id == card.parentNode.parentNode.id) {
-        return true;
-    }
+    // button.parentNode.id
+    if(button.parentNode === card.parentNode.parentNode) return true;
     return false;
 }
 
 function showMore(button) {
 
     var cards = document.querySelectorAll('.card');
+    var showLessButtons = document.querySelectorAll('.show-less-button');
+
+    let counter = 0;
 
     cards.forEach(card => {
         if(sameParent(button, card)) {
             card.classList.remove('hidden');
-            card.classList.add('shown');
+            if(counter > 2) card.classList.add('shown');
+
+            counter++;
+        }
+    });
+
+    // console.log(button.className);
+
+    button.classList.add('hide');
+
+    showLessButtons.forEach(showLessButton => {
+        if(showLessButton.parentNode === button.parentNode) {
+            showLessButton.classList.remove('hide');
+        }
+    });
+}
+
+function showLess(button) {
+
+    var cards = document.querySelectorAll('.card');
+    var showMoreButtons = document.querySelectorAll('.show-more-button');
+
+    cards.forEach(card => {
+        if(sameParent(button, card) && card.classList.contains('shown')) {
+            card.classList.remove('shown');
+            card.classList.add('hidden');
+        }
+    });
+
+    button.classList.add('hide');
+
+    showMoreButtons.forEach(showMoreButton => {
+        if(showMoreButton.parentNode === button.parentNode) {
+            showMoreButton.classList.remove('hide');
         }
     });
 }
